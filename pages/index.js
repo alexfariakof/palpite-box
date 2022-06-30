673f6f3ffe09284869cc831a030e77dd1c09f66d
Ribeiro <alexfariakof@gmail.com>
2022-06-30 00:17:28 -0300

    implementação de leitura e escrita de dados na planilha do google

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import Link from 'next/link'
 
+import useSWR from 'swr'
+import { addRequestMeta } from 'next/dist/server/request-meta';
+
+const fetcher = (...args) => fetch(...args).then(res => res.json());
+
 const Index = () => {
+    const { data, error } = useSWR('/api/get-promo', fetcher);
     return (
         <div >
             <p className='mt-12 text-center'> Nosso objetivo sempre é buscar atender melhor nossos clientes.<br />
@@ -12,9 +18,12 @@ const Index = () => {
                     <a className='bg-blue-400  text-white font-bold py-2 px-4 rounded shadow-lg hover:shadow' > Dar opinião ou sugestão</a>
                 </Link>
             </div>
-            <p className='mt-12 text-center'>
-                Mensagem do Desconto
-            </p>
+            {!data && <p>Carregando...</p>}
+            {data && data.showCupom &&
+                <p className='mt-12 text-center'>
+                    { data.messageCupom }
+                </p>
+            }
         </div>
     )
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,17 @@ module.exports = {
   },
   purge: [],
   theme: {
-    extend: {}
+    animation: {
+      'spin-slow': 'spin 3s linear infinite',
+    },
+    extend: {
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        }
+      }
+    }
   },
   variants: {},
   plugins: []
