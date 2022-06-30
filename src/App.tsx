6e1c73b06aa8bfdb847b55ea66569dba90f2f871
Ribeiro <alexfariakof@gmail.com>
2022-06-30 20:18:24 -0300

    inclusão de theme do Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { BrowserRouter } from "react-router-dom"
+import { AppRoutes } from "./routes";
+import { ThemeProvider } from '@emotion/react'
+import { LightTheme } from "./shared/themes";
 
 export const App = () => {
   return (
-    <div className="App">
-      Testando Incialzando com limpeza
-    </div>
+    <ThemeProvider theme={LightTheme} >
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
